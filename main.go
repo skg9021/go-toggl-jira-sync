b8883ac73959c1185a99a7289b82680a4392ee44
saket <skg9021@gmail.com>
2020-04-05 19:08:34 +0530

    Separating session and config creation from sync logic

--- main.go
+++ main.go
@@ -18,6 +18,45 @@ func init() {
 	godotenv.Load(".env")
 }
 
+func syncTogglJira(session *Session, jiraClient *Client, cfg Config) {
+	for _, entry := range session.getTogglEntries(cfg.Days) {
+		value := getEntryFromDB(entry.Id)
+		duration := strings.Split(value, " ")[0]
+		jiraWorklogID := strings.Split(value, " ")[1]
+
+		description := ""
+
+		// if entry.Task.Name != entry.Description {
+		// 	description = entry.Description
+		// }
+		description = entry.Description
+
+		if duration != strconv.FormatInt(entry.Duration, 10) && entry.JiraId != "" {
+			if jiraWorklogID != "0" {
+				//update
+				start := jira.Time(entry.Start)
+				worklog, error := jiraClient.updateWorkLog(entry.Task.JiraId, description, jiraWorklogID, entry.Duration, start)
+
+				if error == nil {
+					setEntryInDB(entry.Id, strconv.FormatInt(entry.Duration, 10)+" "+worklog.ID)
+				} else {
+					logger.Fatalf("[jira] Task %s error: %v", entry.Task.JiraId, error)
+				}
+			} else {
+				//new
+				start := jira.Time(entry.Start)
+				worklog, error := jiraClient.addWorkLog(entry.JiraId, description, entry.Duration, start)
+
+				if error == nil {
+					setEntryInDB(entry.Id, strconv.FormatInt(entry.Duration, 10)+" "+worklog.ID)
+				} else {
+					logger.Errorf("[jira] Task %s error: %v", entry.Task.JiraId, error)
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 
@@ -31,48 +70,13 @@ func main() {
 
 	defer logger.Init("Logger", false, true, lf).Close()
 
+	config := NewConfig()
+
 	for _, user := range getUsers() {
 		togglSession := getTogglSession(user.TogglToken)
 		session := &Session{togglSession}
 		jiraClient, _ := getJiraClient(user.JiraLogin, user.JiraToken)
-
-		days, _ := strconv.Atoi(os.Getenv("DAYS"))
-
-		for _, entry := range session.getTogglEntries(days) {
-			value := getEntryFromDB(entry.Id)
-			duration := strings.Split(value, " ")[0]
-			jiraWorklogId := strings.Split(value, " ")[1]
-
-			description := ""
-
-			// if entry.Task.Name != entry.Description {
-			// 	description = entry.Description
-			// }
-			description = entry.Description
-
-			if duration != strconv.FormatInt(entry.Duration, 10) && entry.JiraId != "" {
-				if jiraWorklogId != "0" {
-					//update
-					start := jira.Time(entry.Start)
-					worklog, error := jiraClient.updateWorkLog(entry.Task.JiraId, description, jiraWorklogId, entry.Duration, start)
-
-					if error == nil {
-						setEntryInDB(entry.Id, strconv.FormatInt(entry.Duration, 10)+" "+worklog.ID)
-					} else {
-						logger.Fatalf("[jira] Task %s error: %v", entry.Task.JiraId, error)
-					}
-				} else {
-					//new
-					start := jira.Time(entry.Start)
-					worklog, error := jiraClient.addWorkLog(entry.JiraId, description, entry.Duration, start)
-
-					if error == nil {
-						setEntryInDB(entry.Id, strconv.FormatInt(entry.Duration, 10)+" "+worklog.ID)
-					} else {
-						logger.Errorf("[jira] Task %s error: %v", entry.Task.JiraId, error)
-					}
-				}
-			}
-		}
+		syncTogglJira(session, jiraClient, config)
 	}
+
 }
